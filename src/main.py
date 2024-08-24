acb3c30f3e36776c8ed796cf2680856d1b0653af
pappychamp <ka3.no3pappy@gmail.com>
2024-08-24 16:11:24 +0900

    add: cors設定追加

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers import trend
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 全てのオリジンを許可する場合、または特定のオリジンに制限
+    allow_credentials=True,
+    allow_methods=["*"],  # 全てのHTTPメソッドを許可する場合
+    allow_headers=["*"],  # 全てのヘッダーを許可する場合
+)
 app.include_router(trend.router)
