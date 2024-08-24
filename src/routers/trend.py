62252427b94f1e127c52a749618f8f072807260b
pappychamp <ka3.no3pappy@gmail.com>
2024-08-24 16:19:19 +0900

    add: サイト名を指定したときと全サイト指定したときの処理を分岐

--- src/routers/trend.py
+++ src/routers/trend.py
@@ -1,5 +1,6 @@
 from datetime import date
-from typing import List
+from itertools import groupby
+from typing import Dict, List
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,7 +12,10 @@ from src.services.trend import get_trend_data
 router = APIRouter()
 
 
-@router.get("/trend", response_model=List[TrendListResponse])
+@router.get("/trend", response_model=Dict[str, List[TrendListResponse]])
 async def list_trend(site: str, filter_date: date, session: AsyncSession = Depends(get_db)):
     response = await get_trend_data(site, filter_date, session)
-    return response
+    grouped_data = {}
+    for site_name, items in groupby(response, key=lambda x: x.site.name):
+        grouped_data[site_name] = list(items)
+    return grouped_data
--- src/services/trend.py
+++ src/services/trend.py
@@ -14,15 +14,29 @@ async def get_trend_data(site: str, filter_date: date, session: AsyncSession) ->
     tz = pytz.timezone("Asia/Tokyo")
     start_of_day = tz.localize(datetime(filter_date.year, filter_date.month, filter_date.day))
     end_of_day = start_of_day + timedelta(days=1)
-    result: Result = await session.execute(
-        select(TrendData)
-        .join(TrendData.site)
-        .options(joinedload(TrendData.site), joinedload(TrendData.tags))
-        .filter(
-            Site.name == site,
-            TrendData.created_at >= start_of_day,
-            TrendData.created_at < end_of_day,
+    if site == "all":
+        result: Result = await session.execute(
+            select(TrendData)
+            .join(TrendData.site)
+            .options(joinedload(TrendData.site), joinedload(TrendData.tags))
+            .filter(
+                TrendData.created_at >= start_of_day,
+                TrendData.created_at < end_of_day,
+            )
+            .order_by(Site.name, TrendData.ranking)
         )
-    )
+    else:
+        result: Result = await session.execute(
+            select(TrendData)
+            .join(TrendData.site)
+            .options(joinedload(TrendData.site), joinedload(TrendData.tags))
+            .filter(
+                Site.name == site,
+                TrendData.created_at >= start_of_day,
+                TrendData.created_at < end_of_day,
+            )
+            .order_by(TrendData.ranking)
+        )
+
     response = result.scalars().unique().all()
     return response
