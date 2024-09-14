decde6053a5b7782320b0b48c5295ec55c5cb335
pappychamp <ka3.no3pappy@gmail.com>
2024-09-14 23:44:14 +0900

    mod: dbの仕様変更の反映

--- src/schemas/trend.py
+++ src/schemas/trend.py
@@ -15,9 +15,9 @@ class TagModel(BaseModel):
 
 class TrendBase(BaseModel):
     title: str = Field(..., min_length=1, max_length=255, description="Title cannot be empty")
-    ranking: Optional[int] = Field(None, description="Ranking of the item")
+    ranking: int = Field(None, description="Ranking of the item")
     category: Optional[str] = Field(None, max_length=255, description="Category must be a string with a maximum length of 255 characters")
-    published_at: datetime
+    published_at: Optional[datetime] = Field(None, description="The date and time when the item was published.")
     url: Optional[str] = Field(None, max_length=255, description="URL must be a string with a maximum length of 255 characters")
     embed_html: Optional[str] = Field(None, description="EmbedHtml must be a string")
     tags: List[TagModel] | None = []
